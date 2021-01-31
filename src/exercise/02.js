cd120e2f147d7a276bc208e5d16d3d087d8ca120
Orkun Tuzel <orkun.tuzel@onior.com>
2021-01-31 17:46:06 +0100

    React Hooks - 02.js Persistent state

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,23 +3,73 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+const useLocalStorage = (
+  key,
+  initialValue,
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+) => {
+  const [state, setState] = React.useState(() => {
+    const localValue = window.localStorage.getItem(key)
+    if (localValue) {
+      return deserialize(localValue)
+    }
+    return initialValue
+  })
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  const prevKeyRef = React.useRef(key)
+
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current
+
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey)
+      prevKeyRef.current = key
+    }
+
+    window.localStorage.setItem(key, serialize(state))
+  }, [key, serialize, state])
+
+  return [state, setState]
+}
+
+function Greeting({initialName = ''}) {
+  // const getInitialName = () => {
+  //   return window.localStorage.getItem('name') || initialName
+  // }
+  // TODO - refactor useLocalStorage to accept ( key, initial)
+  // const [name, setName] = useLocalStorage('name', initialName)
+  console.log('%c    Child: render start', 'color: MediumSpringGreen')
+  const [key, setKey] = React.useState('name')
+  const [name, setName] = useLocalStorage(key, initialName)
+
+  React.useEffect(() => {
+    setName(name)
+  }, [setName, name])
 
   function handleChange(event) {
     setName(event.target.value)
   }
+
+  function handleClick() {
+    if (key === 'name') {
+      setKey('firstName')
+    } else if (key === 'firstName') {
+      setKey('Name')
+    } else {
+      setKey('name')
+    }
+  }
+  console.log('%c    Child: render end', 'color: red')
+
   return (
     <div>
+      <button type="button" onClick={handleClick}>
+        Change key!
+      </button>
+
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input onChange={handleChange} id="name" value={name} />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
