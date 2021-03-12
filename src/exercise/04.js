d1611a8f480d2a348fcf9dad9e6013bced572c97
Orkun Tuzel <orkun.tuzel@onior.com>
2021-03-12 16:28:55 +0100

    Ex4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -2,40 +2,53 @@
 // http://localhost:3000/isolated/exercise/04.js
 
 import * as React from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+function Board(props) {
+  const {onAddHistory} = props
 
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
+  // Lazy initialization
+  const [squares, setSquares] = useLocalStorageState(
+    'tictactoe',
+    Array(9).fill(null),
+  )
+  // const [squares, setSquares] = React.useState(
+  //   () => JSON.parse(localStorage.getItem('tictactoe')) || Array(9).fill(null),
+  // )
 
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to mutate or directly change state in React.
-    // Doing so can lead to subtle bugs that can easily slip into production.
-    //
-    // 🐨 make a copy of the squares array
-    // 💰 `[...squares]` will do it!)
-    //
-    // 🐨 set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
+  // DERIVE STATES ON THE FLY
+  const nextValue = calculateNextValue(squares)
+  const winner = calculateWinner(squares)
+  const status = calculateStatus(winner, squares, nextValue)
+
+  // NO GOOD APPROACH - REASON BELOW
+  // const [nextValue, setNextValue] = React.useState('X')
+  // const [winner, setWinner] = React.useState(null)
+
+  // THIS APPROACH IS HARD TO MANAGE AND KEEP STATES SYNC TOGETHER
+  // ALSO CAUSES UNNECESSARY RENDERS
+  // React.useEffect(() => {
+  //   setNextValue(calculateNextValue(squares))
+  //   setWinner(calculateWinner(squares))
+  // }, [squares])
+
+  function selectSquare(index) {
+    if (calculateWinner(squares) || squares[index]) {
+      return
+    }
+
+    const newSquares = [
+      ...squares.slice(0, index),
+      nextValue,
+      ...squares.slice(index + 1),
+    ]
+
+    setSquares(newSquares)
+    onAddHistory(newSquares)
   }
 
   function restart() {
-    // 🐨 reset the squares
-    // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
@@ -48,8 +61,7 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -68,16 +80,6 @@ function Board() {
       <button className="restart" onClick={restart}>
         restart
       </button>
-    </div>
-  )
-}
-
-function Game() {
-  return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
     </div>
   )
 }
@@ -121,6 +123,44 @@ function calculateWinner(squares) {
 
 function App() {
   return <Game />
+}
+
+function Game() {
+  const [history, setHistory] = React.useState([])
+
+  const onAddHistory = step => {
+    setHistory([...history, step])
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board onAddHistory={onAddHistory} />
+      </div>
+      <div className="game-board">
+        <GameInfo history={history} />
+      </div>
+    </div>
+  )
+}
+function GameInfo(props) {
+  const {history} = props
+
+  console.log({history})
+
+  return (
+    <div>
+      <ol>
+        {history.map((step, index) => (
+          <li>
+            <button>
+              {index === 0 ? `Go to game start` : `Go to move # ${index}`}
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  )
 }
 
 export default App
